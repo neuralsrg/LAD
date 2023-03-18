1b0848a542985fbda4f4af083a2117d84d7b00a0
neuralsrg <jsetup@hotmail.com>
2023-03-18 20:16:20 +0300

    Update bool vector generation

--- vector_generator.cpp
+++ vector_generator.cpp
@@ -3,15 +3,15 @@
 VectorGenerator::VectorGenerator(float prob) : 
     prob(prob), rd(), generator(rd()), dist(prob) {}
 
-std::shared_ptr<std::vector<char>> VectorGenerator::operator()(size_t length)
+std::shared_ptr<std::vector<u_int8_t>> VectorGenerator::operator()(size_t length)
 {
-    auto v = std::make_shared<std::vector<char>>(length);
+    auto v = std::make_shared<std::vector<u_int8_t>>(length);
     auto set_value = [&](auto &val) { val = VectorGenerator::dist(VectorGenerator::generator); };
     std::for_each((*v).begin(), (*v).end(), set_value);
     return(v);
 }
 
-void VectorGenerator::print_vector(std::shared_ptr<std::vector<char>> v)
+void VectorGenerator::print_vector(std::shared_ptr<std::vector<u_int8_t>> v)
 {
     for(const auto &val : *v) {
         std::cout << int(val) << ' ';
